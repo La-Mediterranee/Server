70c647854d46fc660bd7d1f4638923ca21c2f2b5
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-30 17:43:23 +0200

    types for env vars

---
+++ src/env.d.ts
@@ -0,0 +1,18 @@
+declare namespace NodeJS {
+	export interface ProcessEnv {
+		readonly DISCORD_CLIENT_ID: string;
+		readonly DISCORD_CLIENT_SECRET: string;
+		readonly STRIPE_SECRET: string;
+		readonly STRIPE_WEBHOOK_SECRET: string;
+		readonly GOOGLE_APPLICATION_CREDENTIALS: string;
+		readonly GOOGLE_STORAGE_BUCKET: string;
+	}
+
+	export interface ImportMetaEnv {
+		readonly DISCORD_CLIENT_ID: string;
+		readonly DISCORD_CLIENT_SECRET: string;
+		readonly STRIPE_SECRET: string;
+		readonly STRIPE_WEBHOOK_SECRET: string;
+		readonly GOOGLE_APPLICATION_CREDENTIALS: string;
+	}
+}
